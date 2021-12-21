b9c6e02d5cccae29d641dc148a878a6e6de30dae
Babatunde Sanusi <babatundesanusi@Babatundes-MacBook-Pro.local>
2021-12-21 16:46:50 +0100

    simulating a failure

--- auth/src/routes/users/__test__/current-user.test.ts
+++ auth/src/routes/users/__test__/current-user.test.ts
@@ -1,23 +1,23 @@
-import request from 'supertest';
-import { app } from '../../../app';
+import request from "supertest";
+import { app } from "../../../app";
 
-it('should return details of the current user', async () => {
-  const user = { email: 'test@test.com', password: 'password' };
+it("should return details of the current user", async () => {
+  const user = { email: "test@test.com", password: "password" };
 
   const cookie = await global.signup(user);
 
   const response = await request(app)
-    .get('/api/users/currentuser')
-    .set('Cookie', cookie)
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
     .send()
-    .expect(200);
+    .expect(400);
 
   expect(response.body.currentUser.email).toEqual(user.email);
 });
 
-it('should return null for non signed in user', async () => {
+it("should return null for non signed in user", async () => {
   const response = await request(app)
-    .get('/api/users/currentuser')
+    .get("/api/users/currentuser")
     .send()
     .expect(200);
 
